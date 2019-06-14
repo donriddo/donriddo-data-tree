111a0249c8a49df35986fdacf470090504a17318
Ridwan Taiwo <donriddo@gmail.com>
2019-06-14 22:30:05 +0100

    Add logic to get first child and siblings

--- processor.js
+++ processor.js
@@ -6,62 +6,43 @@ const expectedDataTree = [];
 rawDataTree = rawDataTree.sort((a, b) => a.start - b.start);
 let output = [];
 
-function isChild(parent, child) {
-    return child.start > parent.start;
+function getFirstChild(stack, parent) {
+    return stack.find(d => d.tier === parent.tier && d.start > parent.start);
 }
 
-function isNode(obj1, obj2) {
-    return obj2.tier.startsWith(obj1.tier) && (obj2.start - obj1.start) <= 5;
+function getSiblings(stack, child) {
+    return stack.filter(
+        d => d.tier.startsWith(child.tier) && d.tier !== child.tier
+    );
 }
 
-function buildParentTree(stack) {
-    let pile = [...stack];
-    const parent = pile.shift();
-    parent.children = [];
-    pile.forEach((child) => {
-        if (isChild(parent, child)) parent.children.push(child);
-    });
-    return [pile, parent];
-}
+let parent = rawDataTree.shift();
 
 function buildDataTree(stack, tree) {
-    let trash = [];
-    function isInTrash(el) {
-        const element = trash.find(
-            d => d.start === el.start && d.tier === el.tier
-        );
-        return !!element;
+    let parent = Object.assign({}, tree);
+    let firstChild = getFirstChild(stack, tree);
+    let siblings = [];
+
+    if (!parent.children) parent.children = [];
+    
+    if (firstChild) {
+        siblings = getSiblings(stack, firstChild)
+        parent.children.push(firstChild);
     }
 
-    let pile = [...stack];
-    let obj = {...tree, children: []};
+    if (siblings.length) parent.children = parent.children.concat(siblings);
 
-    pile.forEach((element) => {
-        if (isNode(obj, element)) {
-            obj.children.push(element);
-            trash.push(element);
-        }
-    });
-    pile = pile.filter(el =>  !isInTrash(el));
-
-    return [pile, obj];
+    if (parent.children.length) {
+        parent.children = parent.children.map(
+            child => buildDataTree(stack, child)
+        );
+    }
+    console.log(parent);
+    return parent;
 }
+console.log('initially: ', parent);
 
-let [pile, parent] = buildParentTree(rawDataTree);
-
-function processData(pile, parent) {
-    let tree = {...parent};
-    tree.children.forEach((child) => {
-        let [newPile, obj] = buildDataTree(pile, child);
-        if (obj.children.length) {
-            obj = processData(newPile, obj);
-        }
-        tree.children.push(obj);
-    });
-    return tree;
-}
-
-parent = processData(pile, parent);
+parent = buildDataTree(rawDataTree, parent);
 
 
 fs.writeFileSync('output.json', JSON.stringify(parent, null, 4));
